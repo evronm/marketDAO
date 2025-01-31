72a761d2f8b72004d6455516f2e1a8ef68974197
evronm <evronm@gmail.com>
2025-01-31 17:43:22 -0500

    add proposal factory

---
+++ src/ProposalFactory.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./ProposalTypes.sol";
+
+contract ProposalFactory {
+    MarketDAO public dao;
+    mapping(uint256 => address) public proposals;
+    uint256 public proposalCount;
+
+    constructor(MarketDAO _dao) {
+        dao = _dao;
+    }
+
+    function createResolutionProposal(
+        string memory description
+    ) external returns (ResolutionProposal) {
+        ResolutionProposal proposal = new ResolutionProposal(dao, description);
+        proposals[proposalCount++] = address(proposal);
+        return proposal;
+    }
+
+    function createTreasuryProposal(
+        string memory description,
+        address recipient,
+        uint256 amount,
+        address token,
+        uint256 tokenId
+    ) external returns (TreasuryProposal) {
+        TreasuryProposal proposal = new TreasuryProposal(
+            dao,
+            description,
+            recipient,
+            amount,
+            token,
+            tokenId
+        );
+        proposals[proposalCount++] = address(proposal);
+        return proposal;
+    }
+
+    function createMintProposal(
+        string memory description,
+        address recipient,
+        uint256 amount
+    ) external returns (MintProposal) {
+        MintProposal proposal = new MintProposal(
+            dao,
+            description,
+            recipient,
+            amount
+        );
+        proposals[proposalCount++] = address(proposal);
+        return proposal;
+    }
+
+    function getProposal(uint256 index) external view returns (address) {
+        require(index < proposalCount, "Invalid proposal index");
+        return proposals[index];
+    }
+}
---
+++ test/ProposalFactory.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/ProposalFactory.sol";
+
+contract ProposalFactoryTest is Test {
+    MarketDAO dao;
+    ProposalFactory factory;
+    address proposer = address(0x1);
+    
+    function setUp() public {
+        address[] memory initialHolders = new address[](1);
+        initialHolders[0] = proposer;
+        uint256[] memory initialAmounts = new uint256[](1);
+        initialAmounts[0] = 100;
+        string[] memory treasuryConfig = new string[](2);
+        treasuryConfig[0] = "ETH";
+        treasuryConfig[1] = "ERC20";
+        
+        dao = new MarketDAO(
+            "Test DAO",
+            20,
+            51,
+            100,
+            50,
+            true,
+            treasuryConfig,
+            initialHolders,
+            initialAmounts
+        );
+        
+        factory = new ProposalFactory(dao);
+    }
+    
+    function testCreateResolutionProposal() public {
+        vm.startPrank(proposer);
+        ResolutionProposal proposal = factory.createResolutionProposal("Test Resolution");
+        
+        assertEq(factory.proposalCount(), 1);
+        assertEq(factory.getProposal(0), address(proposal));
+        assertEq(proposal.description(), "Test Resolution");
+    }
+    
+    function testCreateTreasuryProposal() public {
+        vm.startPrank(proposer);
+        TreasuryProposal proposal = factory.createTreasuryProposal(
+            "Test Treasury",
+            address(0x2),
+            1 ether,
+            address(0),
+            0
+        );
+        
+        assertEq(factory.proposalCount(), 1);
+        assertEq(factory.getProposal(0), address(proposal));
+        assertEq(proposal.description(), "Test Treasury");
+        assertEq(proposal.recipient(), address(0x2));
+        assertEq(proposal.amount(), 1 ether);
+    }
+    
+    function testCreateMintProposal() public {
+        vm.startPrank(proposer);
+        MintProposal proposal = factory.createMintProposal(
+            "Test Mint",
+            address(0x2),
+            100
+        );
+        
+        assertEq(factory.proposalCount(), 1);
+        assertEq(factory.getProposal(0), address(proposal));
+        assertEq(proposal.description(), "Test Mint");
+        assertEq(proposal.recipient(), address(0x2));
+        assertEq(proposal.amount(), 100);
+    }
+    
+    function testFailInvalidProposalIndex() public view {
+        factory.getProposal(0);
+    }
+}
