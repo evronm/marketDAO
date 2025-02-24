1bc399a5661018959cc512d627405282fd65e559
evronm <evronm@gmail.com>
2025-02-24 09:34:15 -0500

    merged buyin token option

---
+++ test/TokenPriceProposal.t.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/MarketDAO.sol";
+import "../src/ProposalFactory.sol";
+import "../src/ProposalTypes.sol";
+
+contract TokenPriceProposalTest is Test {
+    MarketDAO dao;
+    ProposalFactory factory;
+    address proposer = address(0x1);
+    address voter1 = address(0x2);
+    address voter2 = address(0x3);
+    uint256 constant INITIAL_TOKEN_PRICE = 0.1 ether;
+    uint256 constant NEW_TOKEN_PRICE = 0.2 ether;
+    
+    function setUp() public {
+        address[] memory initialHolders = new address[](3);
+        initialHolders[0] = proposer;
+        initialHolders[1] = voter1;
+        initialHolders[2] = voter2;
+        
+        uint256[] memory initialAmounts = new uint256[](3);
+        initialAmounts[0] = 100;
+        initialAmounts[1] = 50;
+        initialAmounts[2] = 50;
+        
+        string[] memory treasuryConfig = new string[](1);
+        treasuryConfig[0] = "ETH";
+        
+        dao = new MarketDAO(
+            "Test DAO",
+            20,  // 20% support threshold
+            51,  // 51% quorum
+            100, // max proposal age
+            50,  // election duration
+            true, // allow minting
+            INITIAL_TOKEN_PRICE, // token price
+            treasuryConfig,
+            initialHolders,
+            initialAmounts
+        );
+
+        factory = new ProposalFactory(dao);
+    }
+
+    function testTokenPriceProposal() public {
+        // Verify initial token price
+        assertEq(dao.tokenPrice(), INITIAL_TOKEN_PRICE);
+        
+        // Create proposal to change token price
+        vm.startPrank(proposer);
+        TokenPriceProposal proposal = factory.createTokenPriceProposal(
+            "Change token price", 
+            NEW_TOKEN_PRICE
+        );
+        dao.setApprovalForAll(address(proposal), true);
+        
+        // Add support to trigger election
+        proposal.addSupport(40); // 20% of 200 total tokens needed
+        assertTrue(proposal.electionTriggered());
+        
+        // Vote yes 
+        dao.safeTransferFrom(proposer, proposal.yesVoteAddress(), 1, 100, "");
+        vm.stopPrank();
+        
+        vm.startPrank(voter1);
+        dao.setApprovalForAll(address(proposal), true);
+        dao.safeTransferFrom(voter1, proposal.yesVoteAddress(), 1, 50, "");
+        vm.stopPrank();
+        
+        // Execute the proposal after election period
+        vm.roll(block.number + 50);
+        proposal.execute();
+        
+        // Verify that token price was updated
+        assertEq(dao.tokenPrice(), NEW_TOKEN_PRICE);
+        
+        // Test purchase at new price
+        address buyer = address(0x4);
+        vm.deal(buyer, 1 ether);
+        
+        vm.prank(buyer);
+        dao.purchaseTokens{value: 0.4 ether}();
+        
+        // Should get 2 tokens at the new price
+        assertEq(dao.balanceOf(buyer, 0), 2);
+    }
+    
+    function testSetTokenPriceDirect() public {
+        // This should fail as only a proposal can change the price
+        vm.expectRevert("Only active proposal can set price");
+        dao.setTokenPrice(NEW_TOKEN_PRICE);
+    }
+}
---
+++ test/TokenSale.t.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/MarketDAO.sol";
+
+contract MarketDAOTokenSaleTest is Test {
+    MarketDAO dao;
+    address alice = address(0x1);
+    address bob = address(0x2);
+    uint256 constant TOKEN_PRICE = 0.1 ether;
+    
+    function setUp() public {
+        address[] memory initialHolders = new address[](2);
+        initialHolders[0] = alice;
+        initialHolders[1] = bob;
+        
+        uint256[] memory initialAmounts = new uint256[](2);
+        initialAmounts[0] = 100;
+        initialAmounts[1] = 50;
+        
+        string[] memory treasuryConfig = new string[](2);
+        treasuryConfig[0] = "ETH";
+        treasuryConfig[1] = "ERC20";
+        
+        dao = new MarketDAO(
+            "Test DAO",
+            20,
+            51,
+            100,
+            50,
+            true,
+            TOKEN_PRICE,
+            treasuryConfig,
+            initialHolders,
+            initialAmounts
+        );
+    }
+
+    function testTokenPrice() public {
+        assertEq(dao.tokenPrice(), TOKEN_PRICE);
+    }
+    
+    function testPurchaseTokens() public {
+        address buyer = address(0x3);
+        vm.deal(buyer, 1 ether);
+        
+        uint256 purchaseAmount = 0.5 ether; // Should get 5 tokens
+        uint256 expectedTokens = purchaseAmount / TOKEN_PRICE;
+        
+        vm.prank(buyer);
+        dao.purchaseTokens{value: purchaseAmount}();
+        
+        assertEq(dao.balanceOf(buyer, 0), expectedTokens);
+        assertEq(address(dao).balance, purchaseAmount);
+        
+        // Check that buyer was added to governance token holders
+        address[] memory holders = dao.getGovernanceTokenHolders();
+        bool foundBuyer = false;
+        for(uint i = 0; i < holders.length; i++) {
+            if(holders[i] == buyer) {
+                foundBuyer = true;
+                break;
+            }
+        }
+        assertTrue(foundBuyer);
+    }
+    
+    function testMultiplePurchases() public {
+        address buyer = address(0x3);
+        vm.deal(buyer, 1 ether);
+        
+        vm.startPrank(buyer);
+        
+        dao.purchaseTokens{value: 0.2 ether}();
+        assertEq(dao.balanceOf(buyer, 0), 2);
+        
+        dao.purchaseTokens{value: 0.3 ether}();
+        assertEq(dao.balanceOf(buyer, 0), 5);
+        
+        vm.stopPrank();
+    }
+
+    function testFailPurchaseWithIncorrectAmount() public {
+        address buyer = address(0x3);
+        vm.deal(buyer, 1 ether);
+        
+        // Try to purchase with amount not divisible by token price
+        vm.prank(buyer);
+        dao.purchaseTokens{value: 0.15 ether}();
+    }
+
+    function testFailPurchaseWithZeroPayment() public {
+        vm.prank(address(0x3));
+        dao.purchaseTokens{value: 0}();
+    }
+
+    function testFailPurchaseWhenDisabled() public {
+        // Create new DAO with token price = 0
+        address[] memory initialHolders = new address[](0);
+        uint256[] memory initialAmounts = new uint256[](0);
+        string[] memory treasuryConfig = new string[](0);
+        
+        MarketDAO disabledSalesDao = new MarketDAO(
+            "No Sales DAO",
+            20,
+            51,
+            100,
+            50,
+            true,
+            0, // disable direct sales
+            treasuryConfig,
+            initialHolders,
+            initialAmounts
+        );
+        
+        address buyer = address(0x3);
+        vm.deal(buyer, 1 ether);
+        
+        vm.prank(buyer);
+        disabledSalesDao.purchaseTokens{value: 0.1 ether}();
+    }
+}
