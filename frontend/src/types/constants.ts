1d82bb5c1422878ddd469d0af3a09becfe1c94c4
evronm <evronm@gmail.com>
2025-10-31 17:05:06 -0400

    update contract addresses in front end

--- frontend/src/types/constants.ts
+++ frontend/src/types/constants.ts
@@ -1,6 +1,6 @@
 // Contract addresses (these are from local deployment - should be configurable)
-export const DAO_ADDRESS = '0x0B306BF915C4d645ff596e518fAf3F9669b97016';
-export const FACTORY_ADDRESS = '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1';
+export const DAO_ADDRESS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+export const FACTORY_ADDRESS = '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512';
 
 // Rarible testnet URL template
 export const RARIBLE_TESTNET_URL = (daoAddress: string, tokenId: string) =>
